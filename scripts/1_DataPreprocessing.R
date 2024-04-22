d9015be63e8e11fa4c06e79f42dabebf087ed6bc
keuth <keuth@AG26-NB-02-MINT.ibb.uni-potsdam.de>
2024-04-22 09:49:16 +0200

    fixed study site column

--- scripts/1_DataPreprocessing.R
+++ scripts/1_DataPreprocessing.R
@@ -35,20 +35,20 @@ for (i in 1:length(raw_freshinv_names)) {
   if(any(colnames(raw_freshinv_list[[i]]) == "X")){raw_freshinv_list[[i]] <- raw_freshinv_list[[i]] %>% select(-X)} #removing empty columns
   if(any(colnames(raw_freshinv_list[[i]]) == "X.1")){raw_freshinv_list[[i]] <- raw_freshinv_list[[i]] %>% select(-X.1)}
   if(raw_freshinv_names[i] == "UK_001.csv"){raw_freshinv_list[[i]]$listed.in.freshwaterecology.com <- "NA"}#add missing column to one specific dataset
-  study_site <- str_remove(raw_freshinv_names[i], ".csv") #Extract country and study site from file name
-  study_site <- str_remove(study_site, "00")
-  country <- str_remove(study_site, "\\d+" )
-  raw_freshinv_list[[i]]$country <- str_remove(country, "_" ) #add country as new column
-  raw_freshinv_list[[i]]$study_site <- study_site #add study site of each country as a new column
-  names(raw_freshinv_list)[i] <- study_site #change name of the list element
+  country <- str_remove(raw_freshinv_names[i], ".csv") #Extract country from file name
+  country <- str_remove(country, "\\d+" )
+  country <- str_remove(country, "_" )
+  raw_freshinv_list[[i]]$country <- country #add country as new column
+  raw_freshinv_list[[i]]$study_site <- paste(country, raw_freshinv_list[[i]]$site_id, sep = "_")
+  names(raw_freshinv_list)[i] <- country  #change name of the list element
 }
 
 #add Bulgaria_007 additionally to the list
 raw_freshinv_list[[43]] <- read.csv("data/Bulgaria_007.csv")
 raw_freshinv_list[[43]]$listed.in.freshwaterecology.com <- "NA" #add missing column
 raw_freshinv_list[[43]]$country <- "Bulgaria" #add country
-raw_freshinv_list[[43]]$study_site <- "Bulgaria_7" #add study site
-names(raw_freshinv_list)[43] <- "Bulgaria_7" #change name of the element
+raw_freshinv_list[[43]]$study_site <- paste("Bulgaria", raw_freshinv_list[[43]]$site_id, sep = "_") #add study site
+names(raw_freshinv_list)[43] <- "Bulgaria" #change name of the element
 
 #combine elements of the list to a large data frame --------
 raw_freshinv <- do.call(rbind, raw_freshinv_list)
