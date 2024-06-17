b78bef67e032d8eba6c41cbb46f7df3cdf96c7ab
keuth <keuth@AG26-NB-02-MINT.ibb.uni-potsdam.de>
2024-06-17 15:31:50 +0200

    checked odonata data

--- scripts/4_GlobalRange.R
+++ scripts/4_GlobalRange.R
@@ -5,3 +5,16 @@ library(terra)
 
 # Load data
 odonata <- vect("data/IUCN polygons/FW_ODONATA/FW_ODONATA.shp")
+TREAM <- read.csv("data/TREAM_preprocessed.csv")
+
+names(odonata)
+odonata_names <- unique(odonata$sci_name)
+
+TREAM_odonata <- subset(TREAM, TREAM$Group == "Odonata")
+TREAM_odonata <- TREAM[!is.na(TREAM$taxon),]
+
+polygon_odonata <- TREAM_odonata[which(TREAM_odonata$taxon %in% odonata_names), "taxon"]
+length(unique(polygon_odonata)) #25 species names
+
+odonata_sub <- subset(odonata, odonata$sci_name %in% polygon_odonata)
+
