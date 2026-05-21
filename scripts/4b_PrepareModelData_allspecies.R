881552384516a591c84d5acfdd796258684ca05a
rkeuth <keuth@uni-potsdam.de>
2026-05-21 11:48:14 +0200

    initial commit

---
+++ scripts/4b_PrepareModelData_allspecies.R
@@ -0,0 +1,5 @@
+# Prepare the same code for all selected species
+
+selected_species <- c("Leuctra geniculata", "Simulium noelleri", "Athripsodes aterrimus", "Prodiamesa olivacea", "Baetis vernus", "Hydropsyche angustipennis", 
+                      "Simulium angustitarse", "Simulium trifasciatum", "Brachyptera risi", "Nigrobaetis niger", "Baetis rhodani", "Ephemera danica", "Anabolia furcata", 
+                      "Sialis nigripes", "Anabolia nervosa", "Kageronia fuscogrisea", "Platambus maculatus", "Polycentropus flavomaculatus")
